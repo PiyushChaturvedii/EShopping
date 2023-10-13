71418d10eef026b5aa0e22b4636739857119c6bb
piyush-chaturvedi-au9 <piyush1.chatur@gmail.com>
2023-10-13 17:12:47 +0530

    new Project

---
+++ src/App.js
@@ -0,0 +1,29 @@
+import { Routes, Route, Outlet } from 'react-router-dom';
+import "./categories.styles.scss";
+import Home from "./routes/home/home.component";
+import Navigation from './routes/navigation/navigation.component';
+import Authentication from './routes/authentication/authentication.component';
+import Shop from './routes/shop/shop.component';
+import Checkout from './routes/checkout/checkout.component';
+
+
+// const Shop = () => {
+//   return <h1> I am the shop page </h1>
+// }
+
+
+const App = () => {
+  return (
+    <Routes>
+      <Route path = '/' element = {<Navigation />}>
+        <Route path = 'home' element = {<Home />}/>
+        <Route path = 'shop/*' element = {<Shop />}/>
+        <Route path='auth' element={<Authentication />}/>
+        <Route path='checkout' element={<Checkout />}/>
+
+      </Route>
+  </Routes>
+)
+}
+
+export default App;
